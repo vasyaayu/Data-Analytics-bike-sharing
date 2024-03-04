2c55136acb84acfe7339d791ec7f69fc298c14fd
vasyaayu <126476907+vasyaayu@users.noreply.github.com>
2024-03-04 17:08:49 +0700

    Update streamlit_dashboard.py

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
-import plotly.graph_objects as go
 
 # Set up the page
 st.set_page_config(page_title="Sales Performance Dashboard", page_icon=":bar_chart:", layout="wide")
