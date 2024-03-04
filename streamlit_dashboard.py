4ddbf1b4836271496ecc8d7846c4aa71815ad1e6
vasyaayu <126476907+vasyaayu@users.noreply.github.com>
2024-03-04 22:00:03 +0700

    Delete streamlit_dashboard.py

--- streamlit_dashboard.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-# Set up the page
-st.set_page_config(page_title="Sales Performance Dashboard", page_icon=":bar_chart:", layout="wide")
